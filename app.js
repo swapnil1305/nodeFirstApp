a230a112d4a1a8bbed4be22f9aa34b918f6dbefe
swapnil1305 <swapnil.satpute132@gmail.com>
2023-01-04 13:21:04 +0530

    responseServer

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server= http.createServer((req, res) =>{
-    console.log("swapnil");
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type' , 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body>Welcome to my Node Js project</body>');
+    res.write('</html>');
+    res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
