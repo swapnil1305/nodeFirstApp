45bc013caa5ceac2f3765a52f0b46411565dcfae
swapnil1305 <swapnil.satpute132@gmail.com>
2023-01-04 19:01:22 +0530

    cleanUpCode

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server= http.createServer((req, res) =>{
-    const url = req.url;
-    const method = req.method;
-    if (url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type ="submit">SEND</button></body>');
-        res.write('</html>');
-        return res.end(); 
-    }
-    if (url === '/message'&& method ==='POST'){
-        const body = [];
-        req.on('data',(chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',() => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode =302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-    res.setHeader('Content-Type' , 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body>Welcome to my Node Js project</body>');
-    res.write('</html>');
-    res.end();
-});
+const routes =require('./routes');
+
+console.log(routes.someText);
+
+const server= http.createServer(routes.handler);
 
 server.listen(4000);
