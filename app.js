d0ca28683480c97ae7ec229443b825c69bc3ab1d
swapnil1305 <swapnil.satpute132@gmail.com>
2023-01-13 15:25:37 +0530

    express.JS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from Express!!!</h1>')
+    res.send('<h1>Hello from Express!</h1>')
 });
 
 app.listen(3000);
