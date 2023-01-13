0d74d7a049fef091126110fb6431aff87b7ca4ac
swapnil1305 <swapnil.satpute132@gmail.com>
2023-01-13 22:48:53 +0530

    ParsingRequests

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use(bodyParser.urlencoded({extended: false}));
+
+/* app.use('/',(req, res, next) => {
+    console.log('This always runs!');
     next();
+})
+ */
+app.use('/add-product', (req, res, next) => {
+    //console.log('In another middleware!');
+    res.send('<form action="/product" method="POST"><input type ="text" name="title"><button type="submit">Add Product</button><br><input type= "number" name="size"><button type="submit">Select Size</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>')
+app.use('/product',(req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/', (req, res, next) => {
+    //console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
