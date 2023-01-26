663091bf8c6fdabf4663fe27a879e9374f5f37e4
swapnil1305 <swapnil.satpute132@gmail.com>
2023-01-26 19:53:07 +0530

    shopping1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 
 const express = require('express');
 
@@ -7,14 +8,19 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contactUs');
+const successRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
---
+++ routes/contactUs.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express'); 
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get ('/contactus', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contactUs.html'))
+});
+
+router.post('/contactus',(req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/success');
+});
+
+module.exports = router;
